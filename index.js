dcade739d7679af7f9eb8ad4dfd789e1cf98633e
David Morgenstern <DavidtheConfident@gmail.com>
2021-09-30 16:48:34 -0500

    commit tree 45fe96cdbca47af3e6435579e2c574f86456ca37

--- index.js
+++ index.js
@@ -1,2 +1,47 @@
 
 // Write your code here
+console.log(num1)
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+
+let random = Math.random()
+random = Math.ceil(random)
+random = parseInt(random, 10)
+random = Number.isInteger(random)
+/*if(rand > 0)
+{
+    random = rand
+}
+*/
+
+const max = Math.max(2, 4, 5, 20, 1)
+
+const num3 = 36, num4 = 8
+const mod = num3 % num4
+/*
+function mult(num1, num2){
+    let multiply = num1 * num2
+    
+    return multiply
+}
+
+function rand(){
+    let random = Math.abs(Math.random())
+    return random
+}
+
+function modulus(num3, num4){
+    let mod = num3 % num4
+    return mod
+}
+
+function maxNum(){
+    let max = Math.max()
+    return max
+}
+
+
+//mult(31,2)
+
+*/
